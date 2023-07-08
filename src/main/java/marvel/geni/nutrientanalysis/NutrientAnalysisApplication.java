a6470dc57ad252839fce3e18e0368fd4a83cd9ba
bakhuiwon326 <79272597+bakhuiwon326@users.noreply.github.com>
2023-07-08 12:16:13 +0900

    setting project

---
+++ src/main/java/marvel/geni/nutrientanalysis/NutrientAnalysisApplication.java
@@ -0,0 +1,13 @@
+package marvel.geni.nutrientanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrientAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutrientAnalysisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/marvel/geni/nutrientanalysis/NutrientAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package marvel.geni.nutrientanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrientAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
